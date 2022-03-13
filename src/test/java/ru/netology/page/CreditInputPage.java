b233474c328d4597b92366b549c33d1d6d775998
LidiiaBogdanova <novikova71095@gmail.com>
2022-03-13 12:26:14 +0300

    added tests, first issue

---
+++ src/test/java/ru/netology/page/CreditInputPage.java
@@ -0,0 +1,34 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DebitInputPage {
+
+    private SelenideElement debitHeading = $("heading").shouldHave(Condition.text("Оплата по карте"));
+    private SelenideElement creditHeading = $("heading").shouldHave(Condition.text("Кредит по данным карты"));
+    private SelenideElement cardNumber= $("input__top").shouldHave(Condition.exactText("Номер карты"));
+    private SelenideElement month= $("input__top").shouldHave(Condition.exactText("Месяц"));
+    private SelenideElement year= $("input__top").shouldHave(Condition.exactText("Год"));
+    private SelenideElement cardHolder= $("input__top").shouldHave(Condition.exactText("Владелец"));
+    private SelenideElement cvv= $("input__top").shouldHave(Condition.exactText("CVC/CVV"));
+
+
+    public DebitInputPage() {
+        debitHeading.shouldBe(visible);
+    }
+
+    public DebitInputPage() {
+        creditHeading.shouldBe(visible);
+    }
+
+
+
+
+
+
+}
